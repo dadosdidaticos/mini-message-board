059740cdab62f2bfe27b3c7f93e95756e3c39ef3
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-09-26 02:21:33 -0300

    Adição de esqueleto e site básico

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quadro de Mensagens',messages:messages });
+});
+router.get('/new', function(req, res, next) {
+  res.render('form', {title:"Adicionar nova mensagem"});
+});
+router.post('/new', function(req, res, next) {
+  messages.push({text:req.body.newMessage, user:req.body.user, added: new Date()})
+  res.redirect('/');
+});
+
+module.exports = router;
